27ae8e30b026e729b18c3f3c08f1e322194f4882
Fernando Zuccon <fernandozuccon@outlook.com>
2023-04-28 22:43:42 -0300

    Criado o primeiro código "Helo World"

---
+++ MeuPrimeiroProjeto/MeuPrimeiroProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helo World!");
+            Console.ReadKey();
+        }
+    }
+}
