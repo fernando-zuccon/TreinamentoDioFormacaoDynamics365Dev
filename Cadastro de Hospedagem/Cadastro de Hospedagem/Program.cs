ea51aba61f4f5e35798dabf9e52f63036da1bd1d
Fernando Zuccon <fernandozuccon@outlook.com>
2023-05-01 01:19:10 -0300

    Criando Projeto Hospedagem

---
+++ Cadastro de Hospedagem/Cadastro de Hospedagem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_de_Hospedagem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pessoa> hospedes = new List<Pessoa>();
+
+            Pessoa p1 = new Pessoa( Nome:"Hospede 01");
+        }
+    }
+}
---
+++ Cadastro de Hospedagem/Cadastro de Hospedagem/Suite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_de_Hospedagem
+{
+    public class Suite
+    {
+        public string TipoSuite { get; set; }
+        public int Capacidade { get; set; }
+        public decimal ValorDiaria { get; set; }
+    }
+}
